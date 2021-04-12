3e70266e67af05be02c60fca2ce992cc4260bc4e
rkmod <63541190+rkmod@users.noreply.github.com>
2021-04-12 19:32:29 +0300

    GoLang. Beginner. Task 3: getting  data from the Internet

---
+++ jsonplaceholder/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	posts := string(body)
+	fmt.Printf(posts)
+}
